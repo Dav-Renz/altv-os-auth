d63e09fab7d15c250c0d30845927f0c82407a9aa
Dav-Renz <62567929+Dav-Renz@users.noreply.github.com>
2022-04-20 20:35:59 +0200

    added error messages for account verification

--- server/messages.js
+++ server/messages.js
@@ -1,5 +1,7 @@
 export const MSGS = {
     UNDEFINED: `Username, password, or email is undefined.`,
     EXISTS: `Username or email is already in use.`,
-    INCORRECT: `Username or password was incorrect.`
+    INCORRECT: `Username or password was incorrect.`,
+    ACIVATION: `Contact an Administrator to get your account activated.`,
+    NOTALLOWED: `You are not allowed to play on this server.`
 };
